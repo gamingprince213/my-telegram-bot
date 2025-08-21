02cbb5550fdb5d260d7257893774519227b0974f
gamingprince213 <101305879+gamingprince213@users.noreply.github.com>
2025-08-21 11:50:40 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,17 @@
 from fastapi import FastAPI
 import uvicorn
 from bot import get_application
+import os
+import asyncio
 
 app = FastAPI()
 bot_app = get_application()
 
-# FastAPI endpoint (Render free Web Service এর জন্য)
 @app.get("/")
 def root():
     return {"status": "Bot is running!"}
 
 if __name__ == "__main__":
-    # Polling bot আর HTTP server একসাথে চলবে
-    import asyncio
-
     loop = asyncio.get_event_loop()
     loop.create_task(bot_app.run_polling())
     uvicorn.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
